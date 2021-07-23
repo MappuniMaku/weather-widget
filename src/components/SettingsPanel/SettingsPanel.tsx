d1aeda9ef93fee3c48325118df84da2862889b1f
Grigory Telenok <telenok.grigory@gmail.com>
2021-07-23 11:13:47 +1000

    Add cities on SettingsPanel

--- src/components/SettingsPanel/SettingsPanel.tsx
+++ src/components/SettingsPanel/SettingsPanel.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { AddCityForm } from '../AddCityForm/AddCityForm';
+import { useAppSelector, useAppDispatch } from '../../store/hooks';
+import { removeCity } from '../../store/citiesSlice';
+import { ReactComponent as IconTrash } from '../../assets/icons/trash.svg';
 
-export const SettingsPanel: React.FC = () => {
+import './SettingsPanel.scss';
+
+export const SettingsPanel: React.FC = (props) => {
+    const cities = useAppSelector(state => state.cities.items);
+    const dispatch = useAppDispatch();
+
     return (
-        <div>Настройки</div>
+        <div className="SettingsPanel">
+            <ul>
+                {cities.map(city => (
+                    <li key={city.id}>
+                        <span>{city.name}</span>
+
+                        <button
+                            className="SettingsPanel__removeButton"
+                            type="button"
+                            onClick={() => dispatch(removeCity(city.id))}
+                        >
+                            <IconTrash className="SettingsPanel__removeButtonIcon" />
+                        </button>
+                    </li>
+                ))}
+            </ul>
+
+            <AddCityForm />
+        </div>
     );
 }
